2ba3b115ad9e907560b25493654cc32ac1e55b23
Andrés Ortega <andres03ortega@gmail.com>
2024-06-28 11:55:53 +0200

    rutas videos

--- src/app/components/video-index.jsx
+++ src/app/components/video-index.jsx
@@ -3,8 +3,8 @@
 
 export default () => (
   <div className="header__video">
-    <video className="header__video-desktop" autoPlay loop muted playsInline type="video/mp4" src={require('../assets/video/ermo-proyectos-2024-header-desktop.mp4')} />
-    <video className="header__video-mobile" autoPlay loop muted playsInline type="video/mp4" src={require('../assets/video/ermo-proyectos-2024-mobile-desktop.mp4')} />
+    <video className="header__video-desktop" autoPlay loop muted playsInline type="video/mp4" src="https://soyandres.es/ermo/video/ermo-proyectos-2024-header-desktop.mp4" />
+    <video className="header__video-mobile" autoPlay loop muted playsInline type="video/mp4" src="https://soyandres.es/ermo/video/ermo-proyectos-2024-header-desktop.mp4" />
   </div>
 )
 
